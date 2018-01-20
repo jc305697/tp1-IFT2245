4247729b361a02fdba16e17578fdb5a55fab806c
jeremy coulombe <jeremy.coulombe@hotmail.fr>
2018-01-20 13:07:19 -0500

    boucle commencer Comment parse la string. Est-ce que scan arrête à un espace?

--- ch.c
+++ ch.c
@@ -1,18 +1,33 @@
 /* ch.c.
-auteur:
-date:
-problèmes connus:
+auteur: Maude Sabourin et Jeremy Coulombe
+date: 20 Janvier 2018
+problèmes connus: a date aucun
 
   */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 int main (void)
 {
     fprintf (stdout, "%% ");
 
     /* ¡REMPLIR-ICI! : Lire les commandes de l'utilisateur et les exécuter.   */
+
+    printf("entrer votre commande, pour quitter entrer exit");
+    char entrer[200];
+    scanf( entrer, 200);//200 serait supposer etre la taille d entrer maximmum
+    while (strcmp(entrer,"exit") != 0)
+    {
+
+
+        //switch
+
+        printf("nouvelle commande");  //
+        scanf(entrer, "%200");
+    }
 
     fprintf (stdout, "Bye!\n");
     exit (0);
