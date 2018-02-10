47861a8af88a37cade48c78be774c86bce231add
Maudie <whippou@gmail.com>
2018-02-10 14:31:03 -0500

    Addedcomments

--- ch.c
+++ ch.c
@@ -1,9 +1,6 @@
 /* ch.c.
 auteur: Maude Sabourin et Jeremy Coulombe
-date: 20 Janvier 2018
-problèmes connus: a date aucun
-
-  */
+date: 20 Janvier 2018 */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,8 +19,7 @@ char* getNextValue(char *currentToken){
     return strtok (NULL, " ");
 }
 
-void myFree(char **command, int length)
-{
+void myFree(char **command, int length) {
     for(int i = 0 ; i < length;i++) {
         if (command[i]!= NULL) {
             free(command[i]);
@@ -32,6 +28,7 @@ void myFree(char **command, int length)
     free(command);
 }
 
+//Copy un tableau de pointeurs du début à la fin et rajoute l'element NULL comme flag de fin
 char** copy(char **command,int start,int end) {
     int longueur = end - start +1;
     char **copie = malloc(longueur * sizeof(char *));
@@ -47,29 +44,31 @@ char** copy(char **command,int start,int end) {
     return copie;
 }
 
+//Lit l'entrée de l'utilisation et retourne un token vers le premier mot (la commande)
 char* read_input() {
-    //char* input =NULL;
     char* input;
     size_t taille = 0;
-    //input = (char *) malloc(200 * sizeof(char));
-
-    //Crédit G Praveen Kumar pour le scanf avec espacement
-    //scanf(" %[^\n]s", input);
+    //La taille de l'input est décidée dynamiquement via getline
     getline(&input,&taille,stdin);
     input = strtok(input,"\n");
-    // getline(&input,&taille,stdin);//TODO: remplacer scanf par getline
-    //Sépare chaque partie du string
+
+    //Sépare chaque partie du string selon les espacements
+    //Nous supposons une syntaxe avec un espace entre chaque élément
     char *split = strtok(input, " ");
     return split;
 }
 
-//Prend un pointeur et crée un array de pointeurs pour chaque mot
+//Loop dans les tokens du input et crée un array de pointeurs
 char** parse_input(char *input) {
+    //TODO: Changer le BUFSIZ
     char **parsedArray = (char **) malloc(sizeof(char*)*BUFSIZ);
     int i = 0;
     while (input != NULL) {
+        //Alloue de la mémoire pour les mots de la phrase
         parsedArray[i] = malloc(strlen(input) + 1);
         strcpy(parsedArray[i], input);
+
+        //Va chercher le next token pour accéder au prochain mot
         input = getNextValue(input);
         i++;
     }
@@ -79,15 +78,15 @@ char** parse_input(char *input) {
 
 int longueur = 0;
 
+//TODO: Cette méthode utile?
 char** getParameters(char** arrayInput, int start, int end){
     if (longueur==0){
         while(arrayInput[longueur]!= 0) {
             longueur = longueur + 1;
         }
     }
-
+    //TODO: Changer le bufsiz
     char **temp = (char **) malloc(sizeof(char*)*(BUFSIZ ));
-    // char **temp = (char **) malloc(sizeof(char*)*(strlen(*arrayInput) +1));
     int k=0;
     for (int j = start; j<end;j++){
         temp[k] = arrayInput[j];
@@ -97,17 +96,18 @@ char** getParameters(char** arrayInput, int start, int end){
     return temp;
 }
 
-void remplaceVariable (char **command)
-{ //lit la ligne de commande et remplace les variables identifier par $
+//TODO : Finir de commenter cette méthode
+// Lit la ligne de commande et remplace les variables par leur valeur via les $
+void remplaceVariable (char **command) {
     int mot = 0;
     int lettre;
 
-    while(command[mot] != 0) { //tant que c'est pas la fin de command
+    //On loop dans les mots de la phrase
+    while(command[mot] != 0) {
         lettre = 0;
-        while (command[mot][lettre] != 0) {//tant que c'est pas la fin du mot
-
-
-            if (command[mot][lettre] == '$') {//si je dois remplacer la variable par sa valeur
+        while (command[mot][lettre] != 0) {
+            //Si la lettre est un $, on a une variable
+            if (command[mot][lettre] == '$') {
                 char * referenceOriginal;
                 char *copie = malloc((strlen(command[mot]) + 1) * sizeof(char));
                 if (lettre != 0) {
@@ -115,7 +115,6 @@ void remplaceVariable (char **command)
                     //copie tout ce qui est dans le string avant le $
                 }
                 //S'il y a d'autres variables pas séparées par des espaces
-                //char** nextvar = copy(command, mot, mot+1);//copie le mot
                 char  *nextvar = malloc((strlen(command[mot]) + 1) * sizeof(char) );
                 strcpy(nextvar,command[mot]);
                 //TODO JÉRÉMY : Sais-tu comment je peux accéder au mot? ici quand je mets *nextchar ça me dit ya un token même si yen a pas (avec for j in 1 2 3 ; do echo $i $j ; done)
@@ -164,7 +163,6 @@ void remplaceVariable (char **command)
                     int i = 0;
                     char *stringConcatTemp;
                     char * stringConcat = malloc((strlen(tableauTemp[0]) + 2 + strlen("")) * sizeof(char));
-                    //stringConcat = strcpy(stringConcat,"");
                     stringConcat[0] = '\0';
                     char *stringTemp;
                     while (tableauTemp[i]!=0 && tableauTemp[i+1]!=0) {
@@ -210,9 +208,9 @@ void remplaceVariable (char **command)
     }
 }
 
+//Exécute l'appel via les commandes du path ou la commande spécifique cd
 int execution (char** arrayInput, char** temp) {
     char cd[3] = "cd";
-    //Commande particuliere
     if( strcmp(arrayInput[0],cd) == 0){
         //cd n'a qu'un seul argument
         int retour = chdir(temp[1]);
@@ -229,11 +227,11 @@ int execution (char** arrayInput, char** temp) {
         if (pid == 0){
             //child
             if (temp[0] != NULL){
-                //Pour ls et cat et compagnie
                 value_returned = execvp(arrayInput[0],temp);
                 exit(value_returned);
             }
         }else{
+            //Le parent retourne le code de l'enfant pour valider les erreurs
             wait(&stat);
             return stat;
         }
@@ -246,15 +244,16 @@ struct Return {
     int posPointVirg;
 };
 
+
+//Loop à partir du start jusqu'à la première
 int findNextSplit(int start, char** command, char* val){
-    //Loop until reach the first ;
     while (strcmp(command[start], val) != 0){
         start++;
     }
     return start;
 }
 
-/* Verifie la structure du for s'il y en a un */
+// Verifie la structure du for s'il y en a un
 struct Return checkFor(char **command){
     char *variable = command[1];
     struct Return return1 = {false,0};
@@ -265,21 +264,21 @@ struct Return checkFor(char **command){
     }
 
     if (strcmp(command[2], "in") != 0) {
-        printf("For badly made, need the format For x in range;Do ...; Done");
+        printf("For badly made, need the format For x in range ; Do ... ; Done");
         return return1;
     }
 
     int posPointVirg = findNextSplit(2, command, ";");
 
     if (strcmp(command[posPointVirg + 1], "do") != 0) {
-        printf("For badly made, need the format For x in range;Do ...; Done");
+        printf("For badly made, need the format For x in range ; Do ... ; Done");
         return return1;
     }
     struct Return return2 = {true, posPointVirg};
     return return2;
 }
 
-/* Verifie la fin d'une loop "for" */
+// Verifie la fin d'une loop "for" */
 bool instructionDone(char** command, int start){
     if (strcmp(command[start], "done") != 0){
         return false;
@@ -287,11 +286,11 @@ bool instructionDone(char** command, int start){
     return true;
 }
 
+// Méthode de contrôle qui ne fait qu'appeler l'exécution de la commande
 int exec (char** copie, int start, int end){
     int valeur_retour;
     char **parameters = getParameters (copie, 0, end-start);
     valeur_retour = execution(parameters,parameters);
-    //myFree(copie, end-start);free déjà via pfirst psecond?
     return valeur_retour;
 }
 
@@ -304,31 +303,42 @@ int runFor(char **command, int pos){
     int end;
     int valeur_retour;
 
+    //Permet de looper dans le range donné
     while (i < pos) {
+        //Si la loop contient des variables, on get la première pour start la loop
         if (strstr(command[i],"$")){
             char *valeur = getenv(&command[i][1]);
             command[i] = valeur;
         }
-        start = pos+2;
 
+        start = pos+2; //skip le do et l'espace
+
+        //Affecte la valeur de l'env
         setenv(variable, command[i], overwrite);
 
         char** copie;
+        //Loop jusqu'au premier done trouvé
         while(!instructionDone(command,start)){
             end = findNextSplit(start, command, ";");
+
+            //Vérifie si une prochaine loop est imbriquée après le do
             if (strcmp(command[end+1],"do")==0){
-                //On va chercher le prochain ; et on ajoute un pour le done
+
+                //Skip le premier done pour l'imbrication
                 end = findNextSplit(end+2,command,";")+2;
+
+                //Copie uniquement le for central et l'exécute
                 copie = copy(command,start,end);
                 lireLigne(copie,0,end-start);
             }
+
+            //Copie et exécute le for externe
             copie = copy(command,start,end);
             remplaceVariable(copie);
 
             valeur_retour = exec(copie,start,end);
 
-            if (valeur_retour < 0)
-            {
+            if (valeur_retour < 0) {
                 printf("erreur numero %d dans for \n",valeur_retour);
                 return valeur_retour;
             }
@@ -342,29 +352,36 @@ int runFor(char **command, int pos){
 
 int runAssignation(char **command){
     char *copie = malloc((strlen(command[0])+ 1) * sizeof(char));
-    char *variable; //Contiendra le nom de la variable
-    //Copie pour ne pas modifier la commande initiale
+    char *var_name;
+
+    //Évite la modification commande initiale
     copie = strcpy(copie,command[0]);
-    variable = strtok(copie, "=");
-    char *valeur;
+    var_name = strtok(copie, "=");
+
     int valeur_retour;
-    valeur = strtok(NULL,"="); //Valeur à donner à la variable
+    //Valeur à donner à la variable
+    char* valeur = strtok(NULL,"=");
     int overwrite = 1;
-    valeur_retour = setenv(variable,valeur,overwrite);//Assignation de la valeur dans l'environnement
+
+    //Assignation de la valeur dans l'environnement
+    valeur_retour = setenv(var_name,valeur,overwrite);
     free(copie);
+
     return valeur_retour; //-1 si erreur
 }
 
+//S'occupe de parser la ligne selon les éléments
 int lireLigne(char **command, int start, int end) {
     int i = 0;
     int valeur_retour;
     const char *pour = "for";
 
-    //Check if "for" is the command
+    //Regarde si la commande commence par un for
     if (strstr(command[start], pour)!= NULL) {
-        //Check if for is well made
+        //REgarde si le for est bien construit
         struct Return rtn = checkFor(command);
         if (rtn.format){
+            //Exécute le for
             valeur_retour = runFor(command,rtn.posPointVirg);
             return valeur_retour;
 
@@ -377,7 +394,7 @@ int lireLigne(char **command, int start, int end) {
 
         //Vérifie si la ligne est une assignation
         while(command[start][i]!= 0) {
-            if(command[start][i] == '='){//si assignation en premier
+            if(command[start][i] == '='){
                 valeur_retour = runAssignation(command);
                 return valeur_retour;
             }
@@ -395,10 +412,8 @@ struct concat {
     int type; //0 for &&, 1 for ||
     int pos;
 };
-/*
- * retourne la position du prochain && ou || isNext indique s'il y en a un
- * autre. type laquelle des 2 et pos indique la position.
- */
+
+//Retourne le type de concat et sa position via une loop dans la commande
 struct concat getNextConcat(char** command, int start){
     const char *et = "&&";
     const char *ou = "||";
@@ -418,6 +433,7 @@ struct concat getNextConcat(char** command, int start){
     return ct;
 }
 
+//Loop dans l'array jusqu'à la valeur NULL qui indique la fin de la phrase
 int getLastWord(char** command,int start){
     int temp = start;
     while (command[temp] != NULL){
@@ -429,64 +445,72 @@ int getLastWord(char** command,int start){
 void splitParts(char** command){
     int i = 0;
     int valeur_retour;
-    //const char *pour = "for";
-    //const char *et = "&&";
-    // const char *ou = "||";
-    int mot = 0; //à enlever
+    int mot = 0;
     struct concat res;
+    //Retourne la position et le type du prochain && ou ||
     res = getNextConcat(command,mot);
 
-    if (res.isNext){//si il y a un && ou un ||
+    //S'il existe une prochaine partie
+    if (res.isNext){
         int start = 0;
-        int end = res.pos;//position du &&
-        char** pfirst = copy(command, start, end);//copie avant le &&
+        int end = res.pos;//position du && ou ||
 
+        //Copie la section avant le && ou ||
+        char** pfirst = copy(command, start, end);
+
+        //Valeur de début et de fin
         int starts = res.pos+1;
         int ends = getLastWord(command,starts);
+
+        //Copie la section après le && et ||
         char** psecond = copy(command, starts, ends);
 
-        //End toujours 1 de plus que le final
+        //Exécuter la première partie
         int valeurRetour1 = lireLigne(pfirst,0,end);
-        //65280 is 255 in hexa, child exited normally
+
+        //Si on a un && et que le child existe anormalement (!=0 sauf 65280 qui est 255)
         if (valeurRetour1 != 0 && valeurRetour1!= 65280 && res.type==0){
+            //Désalloque la mémoire et quitte
             myFree(pfirst, end-start);
             myFree(psecond, ends-starts);
             return;
-        }else if (valeurRetour1==0 && res.type==1){
+        //Si on a un || et que le child existe normalement, on sort
+        }else if ((valeurRetour1==0 || valeurRetour1==65280) && res.type==1){
             return;
         }
 
+        //Si on a un autre && ou ||, on resplit récursivement
         if (getNextConcat(psecond,0).isNext){
             splitParts(psecond);
             return;
         }
 
+        //Exécute la deuxième partie
         int valeurRetour2 = lireLigne(psecond,0,ends-end-1);
         if (valeurRetour2 != 0) {
             myFree(pfirst, end-start);
             myFree(psecond, ends-starts);
-            return;//je retourne la 2e partie
+            return;
         }
 
 
     }else{
+        //S'il n'y a pas de && ou ||, on exécute la commande normalement
         int end = getLastWord(command,0);
         lireLigne(command,0,end);
     }
 
 }
 
+
 int main(void) {
     printf("Mini-Shell > ");
-    // char input[200];
     char* res = read_input();
-    // printf(" valeur de input = %s\n",res);
     while (strcmp(res, "exit") != 0) {
+        //Parse l'entree par espace
         char **command = parse_input(res);
+        //Split l'input selon les && et ||
         splitParts(command);
-        //lireLigne(command);
-        //  free(command);
-        // free(res);
         printf("\r\n Mini-Shell > ");
         res = read_input();
     }
